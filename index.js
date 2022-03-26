bd8e3692b9bf5034fd3a5ecc5b351852f3625018
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2022-03-25 20:06:00 -0600

    bug de minusculas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import getPokemon from "./js/getPokemon.js";
 document.addEventListener("submit", e => {
   if (e.target.matches(".caja1_form")) {
   	e.preventDefault();
-  	let query = e.target.input_pokemon.value;
+  	let query = e.target.input_pokemon.value.toLowerCase();
 
   	if (!query) alert("Debes ingresar un valor, NO PUEDE ESTAR VACIO");
   	else{
